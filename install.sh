795516807ab406843e9fdded36a8e5bb07b649d1
Darren Wu <darren.wu@canonical.com>
2018-12-26 19:15:40 +0800

    Add backup shortcut command

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ sudo cp usr/bin/* /usr/bin/
 
 # select the work and press shortcut Alt+t for translating
 # import the shortcut config file
+
+# dconf dump /org/gnome/settings-daemon/plugins/media-keys/ > config/keybindings-translate.dconf
 dconf load /org/gnome/settings-daemon/plugins/media-keys/ < config/keybindings-translate.dconf
